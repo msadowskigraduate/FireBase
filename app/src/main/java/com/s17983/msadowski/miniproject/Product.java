48a0a49e44bf939f5aad4db9f37843cca095c6f0
Michal Sadowski <michal.sadowski@roche.com>
2017-12-12 11:27:43 +0100

    implemented product edit functionality

--- app/src/main/java/com/s17983/msadowski/miniproject/Product.java
+++ app/src/main/java/com/s17983/msadowski/miniproject/Product.java
@@ -1,5 +1,8 @@
 package com.s17983.msadowski.miniproject;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.firebase.database.IgnoreExtraProperties;
 
 import lombok.AllArgsConstructor;
@@ -17,10 +20,42 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Product {
+public class Product implements Parcelable{
     private String productId;
     private String productName;
     private float productPrice;
     private int productQuantity;
     private boolean isBought;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(productId);
+        parcel.writeString(productName);
+        parcel.writeFloat(productPrice);
+        parcel.writeInt(productQuantity);
+        parcel.writeString(String.valueOf(isBought));
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Product createFromParcel(Parcel in) {
+            return new Product(in);
+        }
+
+        public Product[] newArray(int size) {
+            return new Product[size];
+        }
+    };
+
+    public Product(Parcel in) {
+        productId = in.readString();
+        productName = in.readString();
+        productPrice = in.readFloat();
+        productQuantity = in.readInt();
+        isBought = Boolean.getBoolean(in.readString());
+    }
 }
